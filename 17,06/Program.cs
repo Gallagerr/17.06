e53c20c13358ed045564a5bdb89fe07ba943acf6
sadiev <sadiev@itstep.kz>
2019-06-07 21:41:30 +0600

    Добавьте файлы проекта.

---
+++ 17,06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace _17_06
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var threads = new Thread[20];
+      User user = new User
+      {
+        Name = "Saske",
+        BankAccount= new Money
+        {
+          Score = 1000
+        }
+      };
+      foreach (Thread thread in threads)
+      {
+        Thread.Sleep(5 * new Random().Next(100));
+        thread.Start(user);
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
